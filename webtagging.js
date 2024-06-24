2d154ce693d2dea50b4e4bf6449cdf90b4dfcdf2
Connor <134980621+milvus-connor@users.noreply.github.com>
2024-06-24 17:13:35 +0900

    Add files via upload

--- webtagging.js
+++ webtagging.js
@@ -11,15 +11,26 @@ window.addEventListener('load', (event) => {
     })
     SalesforceInteractions.setLoggingLevel(5);
 
+    //특정 이벤트에 해당될 때 사용
+    //아래 예시는 페이지 방문이 일어났을 때, Schema 기반으로 고객 행동 정보 수집
+    //수집 목록 중 방문 시 URL, 
     SalesforceInteractions.sendEvent({
         interaction: {
             name: "Entered Page",
+            euNo: 123456,
+            loginType: "true",
+            deviceType: window.navigator.userAgent,
+            browserOsType: window.navigator.userAgent,
             eventType: "childFundEntered",
-            interactionName: "페이지 방문",
             entered: {
+                location: window.navigator.language,
                 fullUrl: window.location.href,
+                pageDep2: "/depth2",
+                pageDep3: "/depth3",
+                pageDep4: "/depth4",
             }
+            //eventId, category, dateTime, deviceId, sessionId 자동 생성
         }
     });
-    console.log("HI")
+    console.log("05:13ㄴ")
 });
