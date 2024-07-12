5f145abe4a1354ac84fb0971d60f8d43ff906595
Connor <134980621+milvus-connor@users.noreply.github.com>
2024-07-12 13:29:59 +0900

    Add files via upload

--- webtagging.js
+++ webtagging.js
@@ -13,11 +13,11 @@ window.addEventListener('load', (event) => {
     //특정 이벤트에 해당될 때 사용
     //아래 예시는 페이지 방문이 일어났을 때, Schema 기반으로 고객 행동 정보 수집
     //모든 데이터 String Type
-    const titleName = document.querySelector("title")
+    const titleName = document.querySelector("title").innerHTML;
     console.log(titleName);
     SalesforceInteractions.sendEvent({
         interaction: {
-            name: "Entered Page", //Schema의 interactionName에 해당
+            name: titleName, //Schema의 interactionName에 해당
             euNo: 123456,
             loginType: "true",
             deviceType: window.navigator.userAgent,
